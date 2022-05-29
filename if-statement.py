8a6a7d6e08a71c6dde8933076412787aa1a9cb95
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-05-29 12:00:28 +0100

    Add files via upload

---
+++ if-statement.py
@@ -0,0 +1,12 @@
+#Basic Concept of IF Statement
+number = 0
+if number == 1:
+    print("Access Granted")
+else:
+    print("Access Denied")
+#IF Statement using boolean data type
+lightbulb = False
+if lightbulb == True:
+    print("The lightbulb is turned on")
+else:
+    print("The lightbulb is turned off")
